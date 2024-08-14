993b0cc7d64921bdc97d8aa0a05a8b51863e6031
Jota <jota.zehering@gmail.com>
2024-08-13 21:47:09 -0300

    Adicionar projeto MinhaClasse

---
+++ src/MinhaClasse.java
@@ -0,0 +1,9 @@
+public class MinhaClasse {
+    
+public static void main (String [] args) {
+
+    System.out.print ("Hello, world? Haha");    //System me de uma saida, cujo vai ser uma impressão, imprima esse texto para mim
+    
+}
+
+}
